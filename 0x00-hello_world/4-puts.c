491779385996d6d066dd1a1a823c2a4b29b7a3dd
Solomon Bassey <solomonbassey20@gmail.com>
2023-07-13 16:45:54 +0100

    Task {2..4} commits

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+	{
+		const char myString[150] = "\"Programming is like building a multilingual puzzle";
+		puts(myString);
+		return (0);
+	}
