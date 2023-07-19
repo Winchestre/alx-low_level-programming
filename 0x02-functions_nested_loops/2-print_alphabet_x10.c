d6c902141b37f9869041e7cebf4ab1c852dce6e3
Solomon Bassey <solomonbassey20@gmail.com>
2023-07-19 04:50:24 +0100

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for(ch = 'a'; ch <= 'z'; ch++)
+		for(j = 97; j <= 122; j++)
 		{
-			_putchar(ch);
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
