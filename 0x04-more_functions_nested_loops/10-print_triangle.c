86d6c11a88ab269bf55e47dc326470cdb69b2ffd
Solomon Bassey <solomonbassey20@gmail.com>
2023-07-20 19:03:51 +0100

    task 10 fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,18 +9,22 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j < (size - 1); j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(' ');
-		}
-		for (j--; j < size; j++)
-		{
-			_putchar(35);
-		}
-		if (i < (size - 1))
-		{
+			for ((j = size - i); j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			if (i == size)
+			{
+				continue;
+			}
 			_putchar('\n');
 		}
 	}
