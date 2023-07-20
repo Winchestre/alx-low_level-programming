a17079f1f9208a9dd2d4f1cec254ccbe9f3ab0e1
Solomon Bassey <solomonbassey20@gmail.com>
2023-07-20 17:33:35 +0100

    main header update

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -6,3 +9,5 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 int _putchar(char c);
+
+#endif
