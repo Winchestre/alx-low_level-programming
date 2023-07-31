0670438b7c4af07c6285c627038f9d8da5512f8c
Solomon Bassey <solomonbassey20@gmail.com>
2023-07-31 13:52:05 +0100

    initial commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: Array arg
+ * Return: Always 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; j++)
+			_putchar(a[i][j];
+		_putchar('\n');
+	}
+}
