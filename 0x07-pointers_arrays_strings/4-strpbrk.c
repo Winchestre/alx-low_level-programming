4da26b4cf9a53ff447bf405f7b2a3d84f5129b53
Solomon Bassey <solomonbassey20@gmail.com>
2023-07-31 14:37:16 +0100

    task 4 fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,16 +8,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (n = 0; accept[n] != '\0'; n++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (s[i] == accept[n])
-				return (s + i);
+			if (*s == accept[i])
+				return (s);
 		}
+		s++;
 	}
 
-	return (NULL);
+	return ('\0');
 }
