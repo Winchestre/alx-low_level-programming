98748247d19f22bb7d686e452355640862a2c884
Solomon Bassey <solomonbassey20@gmail.com>
2023-08-15 01:34:42 +0100

    0x0E Structures initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @struct dog d: pointer variable of type struct dog
+ * @name: pointer variable
+ * @age: age argument
+ * @owner: pointer variable
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL);
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Dog's basic information
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
