4f293a13efd4e8f0ee9ee5798bd8457d368c2426
Solomon Bassey <solomonbassey20@gmail.com>
2023-08-17 04:11:18 +0000

    Task 3 Initial commit

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - evaluates math operation based on
+ * user defined selection.
+ * @s: char operator argument
+ * Return: pointer to function corresonding to
+ * operation
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (i < 10)
+	{
+		if (s[0] == ops->op[i])
+			break;
+		i++;
+	}
+
+	return (ops[i / 2].f);
+}
