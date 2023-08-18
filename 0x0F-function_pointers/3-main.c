43ed593e06a160645ccb5461b4ae813ff46853d0
Solomon Bassey <solomonbassey20@gmail.com>
2023-08-18 17:10:43 +0000

    bug fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,11 +1,13 @@
 #include "3-calc.h"
+
 /**
  * main - get_op_func has operators correlated wit
  * func signs and funcs from op_func
  * if not 4 arguments, return Error & exit 98
  * if op is null, return Error & exit 99
  * if div or mod 0, return Error & exit 100
- * run calc, input one, operator, input two = pointer res to get_op
+ * run calc, input one, operator, input two = 
+ * pointer res to get_op
  * @argc: arguments
  * @argv: double pointer to arguments
  * Return: 0
