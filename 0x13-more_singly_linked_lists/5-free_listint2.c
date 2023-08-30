ef919a56bfbda39cc2556c2da246f0f18418b099
Solomon Bassey <solomonbassey20@gmail.com>
2023-08-30 03:16:05 +0000

    Update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *actv;
+	
+	if (head)
+	{
+		actv = *head;
+		while ((tmp = actv) != NULL)
+		{
+			actv = actv->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
+ * Return: head node's data.
+ */
+int pop_listint(listint_t **head)
+{
+	int h_node;
+	listint_t *h;
+	listint_t *actv;
+	
+	if (*head == NULL)
+		return (0);
+	
+	actv = *head;
+	h_node = actv->n;
+	h = actv->next;
+	free(actv);
+	*head = h;
+	
+	return (h_node); 
+ }
