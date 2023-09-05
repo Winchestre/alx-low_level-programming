826d9df67d32a11d663e5135bcca040ead748abc
Solomon Bassey <solomonbassey20@gmail.com>
2023-09-05 08:52:53 +0100

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,29 +7,29 @@
  * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
-{ 
-         int fd; 
-         ssize_t nrd, nwr; 
-         char *buf; 
-  
-         if (!filename) 
-                 return (0); 
-  
-         fd = open(filename, O_RDONLY); 
-  
-         if (fd == -1) 
-                 return (0); 
-  
-         buf = malloc(sizeof(char) * (letters)); 
-         if (!buf) 
-                 return (0); 
-  
-         nrd = read(fd, buf, letters); 
-         nwr = write(STDOUT_FILENO, buf, nrd); 
-  
-         close(fd); 
-  
-         free(buf); 
-  
-         return (nwr); 
- }
+{
+	int open_file;
+	ssize_t rd, wr;
+	char *buffr;
+
+	if (!filename)
+		return (0);
+
+	open_file = open(filename, O_RDONLY);
+	if (open_file == -1)
+		return (0);
+
+	buffr = malloc(sizeof(char) * (letters));
+	if (!buffr)
+		return (0);
+
+	rd = read(open_file, buffr, letters);
+	wr = write(STDOUT_FILENO, buffr, rd);
+
+	close(open_file);
+
+	free(buffr);
+
+	return (wr);
+
+}
